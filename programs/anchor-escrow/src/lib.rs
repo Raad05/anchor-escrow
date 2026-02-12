b3d866900e1e859bb233b50313dbff43b92442d9
Raad05 <yamin.raad6109@gmail.com>
2026-02-13 03:17:43 +0600

    initial commit

---
+++ programs/anchor-escrow/src/lib.rs
@@ -0,0 +1,19 @@
+use anchor_lang::prelude::*;
+
+declare_id!("CcbeuvrAc4o6DX2gTrsJvfYreBWJ1TdJhzdkLtzWpBjk");
+
+pub mod instructions;
+pub mod state;
+
+pub use instructions::*;
+pub use state::*;
+
+#[program]
+pub mod anchor_escrow {
+    use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)
+    }
+}
