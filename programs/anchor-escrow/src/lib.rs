bb54d77f9e48073d006cbc35db997207fc817fa4
Raad05 <yamin.raad6109@gmail.com>
2026-02-15 21:12:07 +0600

    tests passed

--- programs/anchor-escrow/src/lib.rs
+++ programs/anchor-escrow/src/lib.rs
@@ -1,12 +1,14 @@
-use anchor_lang::prelude::*;
-
-declare_id!("CcbeuvrAc4o6DX2gTrsJvfYreBWJ1TdJhzdkLtzWpBjk");
-
+pub mod constants;
+pub mod error;
 pub mod instructions;
 pub mod state;
 
+use anchor_lang::prelude::*;
+
+pub use constants::*;
 pub use instructions::*;
 pub use state::*;
+declare_id!("CcbeuvrAc4o6DX2gTrsJvfYreBWJ1TdJhzdkLtzWpBjk");
 
 #[program]
 pub mod anchor_escrow {
@@ -19,5 +21,10 @@ pub mod anchor_escrow {
 
     pub fn refund(ctx: Context<Refund>) -> Result<()> {
         ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
     }
 }
